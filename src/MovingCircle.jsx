e4fd842600bc4ccbe03d39421c05b88b400d752e
Abhinandan Mishra <abhinandanmishra360@gmail.com>
2022-06-23 18:41:13 +0530

    removed drag

--- src/MovingCircle.jsx
+++ src/MovingCircle.jsx
@@ -3,13 +3,6 @@ import { motion } from "framer-motion"
 const MovingCircle = ({skill,color}) => {
   return (
     <motion.div
-    drag
-    dragConstraints={{
-      top: -100,
-      left: -100,
-      right: 100,
-      bottom: 100,
-    }}
     className="ball"
     style={{background: `radial-gradient(circle at 40px 40px, ${color}, #000)`}}
   >
